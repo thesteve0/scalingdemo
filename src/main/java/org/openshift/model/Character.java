748df928799cbf5db23c21e3e986485fa41e51c1
Steven Pousty <spousty@redhat.com>
2014-08-25 20:41:50 -0700

    Deploy

--- src/main/java/org/openshift/model/Character.java
+++ src/main/java/org/openshift/model/Character.java
@@ -17,14 +17,22 @@ public class Character {
     private final int MAX_GOLD = 200;
     private final int MAX_HIT = 9;
 
+    //TODO still need some random ID for the character
+    //TODO give a method where if a name is passed in we give it a name
 
+    public Character(){
 
-    public Character() {
+        this(Double.toString(Math.random()));
+
+    }
+
+    public Character(String name) {
         allAttributes.put("Hit Points", rand.nextInt(MAX_HIT) + 1);
         allAttributes.put("Gold Pieces", rand.nextInt(MAX_GOLD) + 5);
         setAttributes(allAttributes);
         addRaceOrClass("Race", races, allAttributes);
         addRaceOrClass("Class", playerClass, allAttributes);
+        allAttributes.put("Name", name);
 
     }
 
@@ -44,5 +52,36 @@ public class Character {
 
     public HashMap getAllAttributes(){
         return allAttributes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Character character = (Character) o;
+
+        if (goldPieces != character.goldPieces) return false;
+        if (hitPoints != character.hitPoints) return false;
+        if (allAttributes != null ? !allAttributes.equals(character.allAttributes) : character.allAttributes != null)
+            return false;
+        if (attributeNames != null ? !attributeNames.equals(character.attributeNames) : character.attributeNames != null)
+            return false;
+        if (playerClass != null ? !playerClass.equals(character.playerClass) : character.playerClass != null)
+            return false;
+        if (races != null ? !races.equals(character.races) : character.races != null) return false;
+        if (rand != null ? !rand.equals(character.rand) : character.rand != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = rand != null ? rand.hashCode() : 0;
+        result = 31 * result + hitPoints;
+        result = 31 * result + goldPieces;
+        result = 31 * result + (allAttributes != null ? allAttributes.hashCode() : 0);
+        result = 31 * rand.nextInt();
+        return result;
     }
 }
