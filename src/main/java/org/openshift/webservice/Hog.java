9cde3cfdaf26b4db275d79b9c35c07a79f9a9faa
gshipley <gshipley@192.168.0.7>
2014-08-31 12:26:08 -0400

    Eat memory 10mb at a time.

--- src/main/java/org/openshift/webservice/Hog.java
+++ src/main/java/org/openshift/webservice/Hog.java
@@ -4,16 +4,29 @@ import javax.ws.rs.*;
 
 import java.util.Vector;
 
-
 @Path("/eat")
 public class Hog {
 
-	 @GET()
-	 @Produces("text/json")
-	 public Vector EatMemory() {
-		 	Vector v = new Vector<String>();
-		 	v.add(new String("java.lang.OutOfMemoryError"));
-		 	System.out.println("java.lang.OutOfMemoryError");
-		 	return v;
-	 }
+	@GET()
+	@Produces("text/json")
+	@Path("quick")
+	public Vector EatMemorySimulate() {
+		Vector v = new Vector<String>();
+		v.add(new String("java.lang.OutOfMemoryError"));
+		System.out.println("java.lang.OutOfMemoryError");
+		return v;
+	}
+
+	@GET()
+	@Produces("text/json")
+	public void EatMemory() {
+		Vector v = new Vector();
+		while (true) {
+			byte b[] = new byte[10485760];
+			v.add(b);
+			Runtime rt = Runtime.getRuntime();
+			System.out.println("free memory: " + rt.freeMemory());
+		}
+	}
+
 }
