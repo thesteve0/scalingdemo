29d066c3960e181cf17d2124b16ef617f348b5b9
gshipley <gshipley@192.168.0.7>
2014-08-31 12:40:15 -0400

    Print the OutOfMemoryError StackTracke

--- src/main/java/org/openshift/webservice/Hog.java
+++ src/main/java/org/openshift/webservice/Hog.java
@@ -19,12 +19,13 @@ public class Hog {
 		Vector v = new Vector();
 		try {
 			while (true) {
-				byte b[] = new byte[10485760];
+				byte b[] = new byte[10000000];
 				v.add(b);
 				Runtime rt = Runtime.getRuntime();
 				System.out.println("free memory: " + rt.freeMemory());
 			}
 		} catch (java.lang.OutOfMemoryError e) {
+			e.printStackTrace();
 			System.out.println(" java.lang.OutOfMemoryError");
 		}
 	}
